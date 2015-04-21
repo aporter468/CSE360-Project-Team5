1d7cf26ee51f24528eb0017b342fbb77ba269520
Thomas Kajder <tkajder@asu.edu>
2015-04-20 20:37:34 -0600

    Added new api paths to allow extracting all patients for a provider and top surveys for a provider.

--- ESASServer/src/main/java/com/cse360/group5/database/SurveyConnector.java
+++ ESASServer/src/main/java/com/cse360/group5/database/SurveyConnector.java
@@ -2,6 +2,7 @@ package com.cse360.group5.database;
 
 import com.cse360.group5.surveys.SurveyResult;
 
+import java.lang.reflect.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -25,6 +26,48 @@ public class SurveyConnector extends ESASConnector {
 
             // Extract the results
             while (resultSet.next()) {
+                int pain = resultSet.getInt("pain");
+                int drowsiness = resultSet.getInt("drowsiness");
+                int nausea = resultSet.getInt("nausea");
+                int appetite = resultSet.getInt("appetite");
+                int shortnessofbreath = resultSet.getInt("shortnessofbreath");
+                int depression = resultSet.getInt("depression");
+                int anxiety = resultSet.getInt("anxiety");
+                int wellbeing = resultSet.getInt("wellbeing");
+                String comments = resultSet.getString("comments");
+                long timestamp = resultSet.getLong("timestamp");
+
+                SurveyResult surveyResult = new SurveyResult(patientid, pain, drowsiness, nausea,
+                        appetite, shortnessofbreath, depression, anxiety, wellbeing, comments, timestamp);
+                surveys.add(surveyResult);
+            }
+
+        } catch (SQLException e) {
+            // TODO: When does this occur?
+            throw new IllegalArgumentException("TBD", e);
+        } finally {
+            releaseConnection(connection);
+        }
+
+        return surveys;
+    }
+
+    public ArrayList<SurveyResult> getTopSurveys(int providerid) {
+        final String query = "SELECT patients.patientid, pain, drowsiness, nausea, appetite, shortnessofbreath, depression, anxiety, wellbeing, comments, timestamp FROM surveys JOIN patients ON patients.patientid WHERE patients.providerid = ? ORDER BY timestamp DESC LIMIT 10";
+        ArrayList<SurveyResult> surveys = new ArrayList<SurveyResult>();
+        Connection connection = getConnection();
+
+        try {
+            // Set up the sql query
+            PreparedStatement pstat = connection.prepareStatement(query);
+            pstat.setInt(1, providerid);
+
+            // Query and obtain the results
+            ResultSet resultSet = pstat.executeQuery();
+
+            // Extract the results
+            while (resultSet.next()) {
+                int patientid = resultSet.getInt("patientid");
                 int pain = resultSet.getInt("pain");
                 int drowsiness = resultSet.getInt("drowsiness");
                 int nausea = resultSet.getInt("nausea");
--- ESASServer/src/main/java/com/cse360/group5/resources/PatientResource.java
+++ ESASServer/src/main/java/com/cse360/group5/resources/PatientResource.java
@@ -3,15 +3,19 @@ package com.cse360.group5.resources;
 import com.cse360.group5.database.InformationConnector;
 import com.cse360.group5.users.PatientUser;
 import com.cse360.group5.users.ProviderUser;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.restlet.data.Status;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+import java.util.ArrayList;
+
 public class PatientResource extends ServerResource {
 
-    @Get("json")
-    public String getPatientInformation(String jsonRequest) {
+    @Get
+    public String getPatientInformation() {
         InformationConnector informationConnector = new InformationConnector();
         PatientUser patientUser;
 
@@ -20,30 +24,37 @@ public class PatientResource extends ServerResource {
         if (user instanceof PatientUser) {
             patientUser = (PatientUser) user;
         } else if (user instanceof ProviderUser) {
-            ProviderUser providerUser = (ProviderUser) user;
-            if (JSONValidator.validPatientInformationRequest(jsonRequest)) {
-                int patientid;
-                try {
-                    JSONObject jsonObject = new JSONObject(jsonRequest);
-
-                    patientid = jsonObject.getInt("patientid");
-                } catch (JSONException e) {
-                    throw new RuntimeException("The patientid field was not of the correct type");
+            // Retrieve the patientid in the URL
+            Integer patientid;
+            try {
+                Object obj = getRequest().getAttributes().get("patientid");
+                if (obj == null) {
+                    patientid = null;
+                } else {
+                    String patientidStr = obj.toString();
+                    patientid = Integer.valueOf(patientidStr);
                 }
+            } catch (ClassCastException e) {
+                getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+                return "Patient ID in url is not of type Integer";
+            }
 
+            if (patientid != null) {
                 patientUser = informationConnector.getPatientUser(patientid);
-
-                // Check that the patient is one of the provider's patients
-                if (patientUser.getProviderId() != Integer.valueOf(providerUser.getIdentifier())) {
-                    throw new RuntimeException("The requested patient is not one of your patients");
-                }
             } else {
-                throw new RuntimeException("Request is not json or does not contain patientid field");
+                ProviderUser providerUser = (ProviderUser) user;
+                ArrayList<PatientUser> patients = informationConnector.getProvidersPatients(Integer.valueOf(providerUser.getIdentifier()));
+                return patientsToJSON(patients).toString();
             }
         } else {
-            throw new RuntimeException("User is not instance of either type. Should not happen");
+            getResponse().setStatus(Status.CLIENT_ERROR_PRECONDITION_FAILED);
+            return "User is not a provider or patient.";
         }
 
+        return patientToJSON(patientUser).toString();
+    }
+
+    private JSONObject patientToJSON(PatientUser patientUser) {
         // Populate the patient information jsonResponse
         JSONObject jsonResponse = new JSONObject();
         try {
@@ -56,8 +67,23 @@ public class PatientResource extends ServerResource {
             throw new RuntimeException("Could not add field to patient json");
         }
 
-        return jsonResponse.toString();
+        return jsonResponse;
     }
 
-    // GET PATIENTS ASSIGNED TO DOCTOR
+    private JSONObject patientsToJSON(ArrayList<PatientUser> patientUsers) {
+        // Populate the patient information jsonResponse
+        JSONObject jsonResponse = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        try {
+            for (PatientUser patientUser: patientUsers) {
+                JSONObject patientJSON = patientToJSON(patientUser);
+                jsonArray.put(patientJSON);
+            }
+            jsonResponse.put("patients", jsonArray);
+        } catch (JSONException e) {
+            throw new RuntimeException("Could not add field to patient json");
+        }
+
+        return jsonResponse;
+    }
 }
