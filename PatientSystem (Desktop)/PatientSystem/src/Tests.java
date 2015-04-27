4564400be6433a98c747ede5589eff418cfc74f4
NicholasCoTran <natran1@asu.edu>
2015-04-27 15:40:01 -0700

    Update Tests.java

--- PatientSystem (Desktop)/PatientSystem/src/Tests.java
+++ PatientSystem (Desktop)/PatientSystem/src/Tests.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 import org.junit.Ignore;
 import static org.junit.Assert;
-import java.lang.Object;
-import java.lang.Enum<KeyCode>;
 
 public class DirectoryTest {
 
@@ -14,7 +12,14 @@ Directory dir = new Directory();
 public void findUser() {
     if(dir.addPatient("a", "b", "c", "d", "e")) {
        int x = dir.findPatientByName("a");
-       assertEquals("Patient not Found by Name", -1, x);
+       if(x!= -1)
+       {
+           boolean found = true;
+           assertTrue("Patient a is Found", found);
+       }
+       assertEquals("Patient a not Found by Name", -1, x);
+       int y = dir.findPatientByName("b");
+       assertEquals("Patient b not Found by Name", -1, y);
     }
 }
 
@@ -24,7 +29,14 @@ public void findUser() {
 public void findUserName() {
     if(dir.addPatient("a", "b", "c", "d", "e")) {
         int x = dir.findPatient("b");
-        assertEquals("Patient not Found by UserName", -1, x);
+        if(x!= -1)
+        {
+           boolean found = true;
+           assertTrue("Patient a is Found", found);
+        }
+        assertEquals("Patient a not Found by UserName", -1, x);
+        int y = dir.findPatient("c");
+        assertEquals("Patient b not Found by Name", -1, y);
     }
 }
 
@@ -34,7 +46,9 @@ public void findUserName() {
 public void doctorExists() {
     if(dir.addDoctor("a", "b", "c", "d", "e")) {
         boolean x = dir.doctorExists("a");
-        assertTrue("Doctor added properly", x);
+        assertTrue("Doctor a exists", x);
+        boolean y = dir.doctorExists("b");
+        assertFalse("Doctor b does not exist", y)'
     }
 }
 
@@ -44,36 +58,15 @@ public void doctorExists() {
 public void findDoctor() {
     if(dir.addDoctor("a", "b", "c", "d", "e")) {
         int x = dir.findDoctor("a");
-        assertEquals("Doctor not Added properly", -1, x);
+        if(x!= -1)
+        {
+           boolean found = true;
+           assertTrue("Doctor a is Found", found);
+        }
+        assertEquals("Doctor a not found", -1, x);
+        int y = dir.findDoctor("b");
+        assertEquals("Doctor b not found", -1, y);
     }
 }
 
-@Test
-
-//Tests GUI Features
-public void guitTest() {
-    Robot rob = new Robot();
-    if(dir.addDoctor("a", "b", "c", "d", "e")) {
-        rob.mouseMove(dir.getLocationUserName());
-        rob.mousePress(InputEvent.BUTTON1_MASK);
-        rob.mouseRelease(InputEvent.BUTTON1_MASK);
-        rob.keyPress(A);
-        rob.keyRelease(A);
-
-        rob.mouseMove(dir.getLocationPassword());
-        rob.mousePress(InputEvent.BUTTON1_MASK);
-        rob.mouseRelease(InputEvent.BUTTON1_MASK);
-        rob.keyPress(B);
-        rob.keyRelease(B);
-
-        rob.mouseMove(dir.getLocationLogin());
-        rob.mousePress(InputEvent.BUTTON1_MASK);
-        rob.mouseRelease(InputEvent.BUTTON1_MASK);
-
-        dir.addSurvey();
-        rob.mouseMove(dir.getLocationHistory());
-        rob.mousePress(InputEvent.BUTTON1_MASK);
-        rob.mouseRelease(InputEvent.BUTTON1_MASK);
-    }
-}
 }
